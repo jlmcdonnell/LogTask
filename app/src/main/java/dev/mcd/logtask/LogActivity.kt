9197ab639a42c6855ede3f067572341b9e984c2a
John McDonnell <john+git@mcd.dev>
2023-05-03 19:23:35 +0100

    Create project

---
+++ app/src/main/java/dev/mcd/logtask/LogActivity.kt
@@ -0,0 +1,11 @@
+package dev.mcd.logtask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LogActivity : ComponentActivity(R.layout.activity_log) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
