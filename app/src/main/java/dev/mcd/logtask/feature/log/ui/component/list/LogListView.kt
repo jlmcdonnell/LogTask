15728c2ae2d6d432eb5fd22f1aaf2c8b873877e2
John McDonnell <john+git@mcd.dev>
2023-05-04 10:15:49 +0100

    Connect adapter to log list view

--- app/src/main/java/dev/mcd/logtask/feature/log/ui/component/list/LogListView.kt
+++ app/src/main/java/dev/mcd/logtask/feature/log/ui/component/list/LogListView.kt
@@ -3,6 +3,7 @@ package dev.mcd.logtask.feature.log.ui.component.list
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.RecyclerView
 import dev.mcd.logtask.R
 
 class LogListView @JvmOverloads constructor(
@@ -11,7 +12,11 @@ class LogListView @JvmOverloads constructor(
     defStyleAttr: Int = 0,
 ) : FrameLayout(context, attributeSet, defStyleAttr) {
 
+    private val adapter = LogListAdapter()
+    private val listView by lazy { findViewById<RecyclerView>(R.id.listView) }
+
     init {
         inflate(context, R.layout.log_list_view, this)
+        listView.adapter = adapter
     }
 }
