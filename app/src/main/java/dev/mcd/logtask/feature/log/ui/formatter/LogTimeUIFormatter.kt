63fbb55e1a1fec6a60320b1009fbff48349d615b
John McDonnell <john+git@mcd.dev>
2023-05-04 14:38:35 +0100

    Update log time UI formatter

--- app/src/main/java/dev/mcd/logtask/feature/log/ui/formatter/LogTimeUIFormatter.kt
+++ app/src/main/java/dev/mcd/logtask/feature/log/ui/formatter/LogTimeUIFormatter.kt
@@ -1,5 +1,12 @@
 package dev.mcd.logtask.feature.log.ui.formatter
 
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+import java.util.Locale
 
-val logTimeUIFormatter = DateTimeFormatter.ISO_TIME
+val logTimeUIFormatter: DateTimeFormatter = DateTimeFormatterBuilder()
+    .appendValue(ChronoField.HOUR_OF_DAY)
+    .appendLiteral(":")
+    .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+    .toFormatter(Locale.UK)
