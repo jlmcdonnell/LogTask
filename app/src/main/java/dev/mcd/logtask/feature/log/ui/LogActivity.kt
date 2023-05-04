2cff0b327ae74fa83f3a2bd123f0a312d3a3a50b
John McDonnell <john+git@mcd.dev>
2023-05-04 12:47:04 +0100

    Add entry point annotation to LogActivity

--- app/src/main/java/dev/mcd/logtask/feature/log/ui/LogActivity.kt
+++ app/src/main/java/dev/mcd/logtask/feature/log/ui/LogActivity.kt
@@ -2,8 +2,10 @@ package dev.mcd.logtask.feature.log.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
 import dev.mcd.logtask.R
 
+@AndroidEntryPoint
 class LogActivity : ComponentActivity(R.layout.activity_log) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
